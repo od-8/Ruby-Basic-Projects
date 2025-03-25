4b5fa1a7327c76dc4180c94095d30385f619cf6f
od-8 <178620777+od-8@users.noreply.github.com>
2025-03-25 20:42:03 +0000

    feat: Completed tests for caesar_cypher.rb

--- caeser_cypher.rb
+++ caeser_cypher.rb
@@ -1,16 +1,35 @@
+# def to_ascii(input, key)
+#   new_str = ''
+#   input_arr = input.split('')
+#   input_arr.each do |num|
+#     if num.ord != 32 && num.ord > 64 && num.ord < 91 # Checks if the number is upercase in ascii values
+#       num = ((((num.ord + key) + 65) % 26) + 65)
+#       new_str += num.chr
+#     end
+#     if num.ord != 32 && num.ord > 96 && num.ord < 123 # Check if the number is lowercase in ascii values
+#       num = (((num.ord - 97 + key) % 26) + 97)
+#       new_str += num.chr
+#     end
+#     new_str += ' ' if num.ord == 32 # Adds a space cause 32 in ascii is space
+#   end
+#   puts "Your string '#{input}' in ascii is '#{new_str}'"
+# end
+
+# The above method was the original one I made close to when i started TOP
+# When creating rspec tests for it i realized it didn't work for puncutation
+# The new better code is below, could still be improved though
+
 def to_ascii(input, key)
-  new_str = ''
   input_arr = input.split('')
-  input_arr.each do |num|
-    if num.ord != 32 && num.ord > 64 && num.ord < 91 # Checks if the number is upercase in ascii values
-      num = ((((num.ord + key) + 65) % 26) + 65)
-      new_str += num.chr
+  output_arr = []
+  input_arr.each do |char|
+    if char.ord.between?(65, 90) #|| character.ord.between?(97, 122)
+      output_arr << ((((char.ord + key) + 65) % 26) + 65).chr
+    elsif char.ord.between?(97, 122)
+      output_arr << (((char.ord - 97 + key) % 26) + 97).chr
+    else
+      output_arr << char.chr
     end
-    if num.ord != 32 && num.ord > 96 && num.ord < 123 # Check if the number is lowercase in ascii values
-      num = (((num.ord - 97 + key) % 26) + 97)
-      new_str += num.chr
-    end
-    new_str += ' ' if num.ord == 32 # Adds a space cause 32 in ascii is space
   end
-  puts "Your string '#{input}' in ascii is '#{new_str}'"
+  return output_arr.join('')
 end
--- spec/caesar_cypher_spec.rb
+++ spec/caesar_cypher_spec.rb
@@ -1,15 +1,69 @@
 require_relative '../caeser_cypher'
 
 describe '#to_ascii' do
-  context 'Input string with a shift key' do
-    it 'input string is hello and shift key is 3' do
-      shift_key =  3
-      string = 'Hello'
-      output_msg = "Your string 'Hello' in ascii is 'Khoor'"
+  context 'When string is empty' do 
+    it 'input string is empty and shift key is 5' do
+      string = ''
+      shift_key = 5 # Random number
+      cypher = ''
 
-      expect(self).to receive(:puts).with(output_msg)
+      result = to_ascii(string, shift_key)
+      expect(result).to eq(cypher)
+    end
+  end
 
-      to_ascii(string, shift_key)
+  context 'When string only has letters' do 
+    it "input string is 'Hello World' and shift key is 3" do
+      string = 'Hello World'
+      shift_key = 3 # Random number
+      cypher = 'Khoor Zruog'
+
+      result = to_ascii(string, shift_key)
+      expect(result).to eq(cypher)
+    end
+  end
+
+  context 'When string has letters, nums and punctuation' do 
+    it "input string is 'Hello World! 123' and shift key is 29" do
+      string = 'Hello World! 123'
+      shift_key = 29 # Random number
+      cypher = 'Khoor Zruog! 123'
+
+      result = to_ascii(string, shift_key)
+      expect(result).to eq(cypher)
+    end
+  end
+
+  context 'When string has only punctuation' do 
+    it "input string is '@!£$.,/?;' and shift key is 9" do
+      string = '@!£$.,/?;'
+      shift_key = 9 # Random number
+      cypher = '@!£$.,/?;'
+
+      result = to_ascii(string, shift_key)
+      expect(result).to eq(cypher)
+    end
+  end
+
+  context 'When string has only numbers' do 
+    it "input string is '0123456789' and shift key is 4" do
+      string = '0123456789'
+      shift_key = 4 # Random number
+      cypher = '0123456789'
+
+      result = to_ascii(string, shift_key)
+      expect(result).to eq(cypher)
+    end
+  end
+
+  context 'When string has punctuation and numbers' do 
+    it "input string is '(0!23$@67&9)' and shift key is 100" do
+      string = '(0!23$@67&9'
+      shift_key = 100 # Random number
+      cypher = '(0!23$@67&9'
+
+      result = to_ascii(string, shift_key)
+      expect(result).to eq(cypher)
     end
   end
 end
